02701ae030e79afcd1fdbd6a513e8da6bb3a7834
Alex Mariano <amarianocello@gmail.com>
2022-04-17 13:12:51 -0700

    Added textReader scanner to read form data_random.txt - Alec

--- src/DriverHeap.java
+++ src/DriverHeap.java
@@ -1,7 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class DriverHeap {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException {
 
+        File data = new File("src/data_random.txt");
+        Scanner textReader = new Scanner(data);
+        while (textReader.hasNext()){
+            //add this to heap using intial method
+        }
 
         System.out.println("Heap built using optimal method: "); //call getFirst10()
         System.out.println("Number of swaps in the heap creation: ");
@@ -11,6 +20,11 @@ public class DriverHeap {
 
         System.out.println ("Heap after 10 removals: ");
 
+
+        textReader = new Scanner(data);
+        while (textReader.hasNext()){
+            //add this to heap using optimal method
+        }
         System.out.println("Heap built using optimal method: "); //call getFirst10()
         System.out.println("Number of swaps in the heap creation: ");
         for (int i = 0; i < 10; i++){
