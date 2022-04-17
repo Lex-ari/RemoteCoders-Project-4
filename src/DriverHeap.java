288236e91c8f9c04a15be5d441eb967b227ea981
Alex Mariano <amarianocello@gmail.com>
2022-04-17 13:03:38 -0700

    Added demo code to DriverHeap.java

--- src/DriverHeap.java
+++ src/DriverHeap.java
@@ -2,5 +2,28 @@ public class DriverHeap {
 
     public static void main(String[] args){
 
+
+        System.out.println("Heap built using optimal method: "); //call getFirst10()
+        System.out.println("Number of swaps in the heap creation: ");
+        for (int i = 0; i < 10; i++){
+            //do remove 10() times
+        }
+
+        System.out.println ("Heap after 10 removals: ");
+
+        System.out.println("Heap built using optimal method: "); //call getFirst10()
+        System.out.println("Number of swaps in the heap creation: ");
+        for (int i = 0; i < 10; i++){
+            // do remove() 10 times
+        }
+        System.out.println ("Heap after 10 removals: ");
+
+    }
+
+
+    public static String getFirst10(ArrayHeap heap){
+        // code that traverses through the array and retrives the first 10 elements.
+
+        return "not finished";
     }
 }
