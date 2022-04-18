f3b255983da13196030514aa1054d0cb3438e77d
Koki <kyamaguchi@cpp.edu>
2022-04-18 16:32:58 -0700

    add array list into the file and created the array

--- src/DriverHeap.java
+++ src/DriverHeap.java
@@ -1,27 +1,35 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
-
 public class DriverHeap {
 
     public static void main(String[] args) throws FileNotFoundException {
 
         File data = new File("src/data_random.txt");
         Scanner textReader = new Scanner(data);
-        while (textReader.hasNext()){
-            //add this to heap using intial method
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        while(textReader.hasNext()){
+            //add this to heap using initial method
+            Integer i = textReader.nextInt();
+            arr.add(i);
         }
 
+
+
         System.out.println("Heap built using optimal method: "); //call getFirst10()
-        System.out.println("Number of swaps in the heap creation: ");
+        System.out.println("Number of swaps in the heap creation: " );
         for (int i = 0; i < 10; i++){
             //do remove 10() times
+
         }
 
         System.out.println ("Heap after 10 removals: ");
 
 
-        textReader = new Scanner(data);
+        //sorted data
+        File data2 = new File("src/data_sorted.txt");
+        textReader = new Scanner(data2);
         while (textReader.hasNext()){
             //add this to heap using optimal method
         }
