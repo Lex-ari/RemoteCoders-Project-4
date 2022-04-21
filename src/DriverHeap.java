2e1225c38080430e49ca1522fb371dd19efaa943
Alex Mariano <amarianocello@gmail.com>
2022-04-20 18:58:51 -0700

    Added output text file and Filewriter- Alex

--- src/DriverHeap.java
+++ src/DriverHeap.java
@@ -1,13 +1,16 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class DriverHeap {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
 
         File data = new File("src/data_random.txt");
+        FileWriter output = new FileWriter("output.txt", false);
         Scanner textReader = new Scanner(data);
         MaxHeap<Integer> simpleHeap = new MaxHeap(); // default capcaity
         while(textReader.hasNext()){
@@ -16,13 +19,16 @@ public class DriverHeap {
             simpleHeap.add(i);
         }
 
-        System.out.println("Heap built using optimal method: " + getFirst10(simpleHeap)); //call getFirst10()
+        System.out.println("Heap built using add method: " + getFirst10(simpleHeap)); //call getFirst10()
+        output.write("Heap built using add method: " + getFirst10(simpleHeap) + "\n"); //call getFirst10()
         System.out.println("Number of swaps in the heap creation: " );
+        output.write("Number of swaps in the heap creation: " + "\n");
         for (int i = 0; i < 10; i++){
             simpleHeap.removeMax();
         }
 
-        System.out.println ("Heap after 10 removals: ");
+        System.out.println ("Heap after 10 removals: " + getFirst10(simpleHeap));
+        output.write("Heap after 10 removals: " + getFirst10(simpleHeap) + "\n");
 
         ArrayList<Integer> arr2 = new ArrayList<Integer>();
         File data2 = new File("src/data_random.txt");
@@ -36,12 +42,15 @@ public class DriverHeap {
         optimalHeap = new MaxHeap(arr2.toArray(new Integer[0]));
 
         System.out.println("Heap built using optimal method: " + getFirst10(optimalHeap)); //call getFirst10()
+        output.write("Heap built using optimal method: " + getFirst10(optimalHeap) + "\n"); //call getFirst10()
         System.out.println("Number of swaps in the heap creation: ");
+        output.write("Number of swaps in the heap creation: " + "\n");
         for (int i = 0; i < 10; i++){
-            // do remove() 10 times
+            optimalHeap.removeMax();
         }
-        System.out.println ("Heap after 10 removals: ");
-
+        System.out.println ("Heap after 10 removals: " + getFirst10(optimalHeap));
+        output.write("Heap after 10 removals: " + getFirst10(optimalHeap) + "\n");
+        output.close();
     }
 
     public static String getFirst10(MaxHeap heap){
